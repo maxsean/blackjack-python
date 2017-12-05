f692c8a30d9cd02894e7993037e93d2b012c8886
maxsean <xsma1990@gmail.com>
2017-12-05 11:34:10 -0500

    added deck class

--- deck.py
+++ deck.py
@@ -1,7 +1,22 @@
 from card import Card
+from random import shuffle
 
 class Deck:
 
-    def get_deck(self):
+    def __init__(self):
+        popped = []
+        self.cards = [Card(face, suit) for face in FACES for suit in SUITS]
+        self.shuffle()
 
-        return [Card(face, suit) for face in FACES for suit in SUITS]
+    def __str__(self):
+        s = ''
+        for c in self.cards:
+            s = s + str(c) + ' '
+        return s
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def deal_card(self):
+        popped = self.cards.pop(0)
+        return popped
